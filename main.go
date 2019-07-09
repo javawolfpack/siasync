7d68f9bb48efc3190867c9bb549732d3acd4daa0
Bryan Dixon <javawolfpack@gmail.com>
2019-07-09 08:59:23 -0700

    Going to ignore the go daemon approach for now and get the yaml multiple destination sync to work

--- main.go
+++ main.go
@@ -6,14 +6,14 @@ import (
   "io/ioutil"
   "log"
   "os"
-  "os/signal"
-  "syscall"
-  "time"
+  // "os/signal"
+  // "syscall"
+  // "time"
   "strings"
 
   "gitlab.com/NebulousLabs/Sia/build"
   sia "gitlab.com/NebulousLabs/Sia/node/api/client"
-  "github.com/takama/daemon"
+  // "github.com/takama/daemon"
   "gopkg.in/yaml.v2"
 )
 
@@ -32,16 +32,16 @@ var (
   parityPieces      uint64
 )
 
-//    dependencies that are NOT required by the service, but might be used
-var dependencies = []string{"dummy.service"}
-
-const (
-
-    // name of the service
-    name        = "SiaSync"
-    description = "Sia Synchronization Service"
-
-)
+// //    dependencies that are NOT required by the service, but might be used
+// var dependencies = []string{"dummy.service"}
+//
+// const (
+//
+//     // name of the service
+//     name        = "SiaSync"
+//     description = "Sia Synchronization Service"
+//
+// )
 
 
 type YamlSync struct {
@@ -62,16 +62,16 @@ type Sync struct {
 
 
 
-// Service has embedded daemon
-type Service struct {
-    daemon.Daemon
-}
+// // Service has embedded daemon
+// type Service struct {
+//     daemon.Daemon
+// }
 
 var stdlog, errlog *log.Logger
 
 func Usage() {
-  fmt.Printf(`usage: siasync <flags> <directory-to-sync>
-  for example: ./siasync -password abcd123 /tmp/sync/to/sia
+  fmt.Printf(`usage: siasync <flags>
+  for example: ./siasync -password abcd123
 
 `)
   flag.PrintDefaults()
@@ -134,77 +134,6 @@ func testConnection(sc *sia.Client) {
 
 }
 
-func GoogleIt(){
-  for {
-    f, err := os.OpenFile("test.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-    if err != nil {
-        log.Fatal(err)
-    }
-    if _, err := f.Write([]byte("Hi\n")); err != nil {
-        log.Fatal(err)
-    }
-    if err := f.Close(); err != nil {
-        log.Fatal(err)
-    }
-    time.Sleep(time.Second)
-  }
-}
-// Manage by daemon commands or run the daemon
-func (service *Service) Manage() (string, error) {
-
-  usage := "Usage: siasync -daemon install | remove | start | stop | status"
-
-  // if received any kind of command, do it
-  if len(os.Args) > 2 {
-      command := os.Args[2]
-      stdlog.Println(command)
-      switch command {
-      case "install":
-          return service.Install()
-      case "remove":
-          return service.Remove()
-      case "start":
-          return service.Start()
-      case "stop":
-          return service.Stop()
-      case "status":
-          return service.Status()
-      default:
-          return usage, nil
-      }
-  }
-  // Set up channel on which to send signal notifications.
-  // We must use a buffered channel or risk missing the signal
-  // if we're not ready to receive when the signal is sent.
-  interrupt := make(chan os.Signal, 1)
-  signal.Notify(interrupt, os.Interrupt, os.Kill, syscall.SIGTERM)
-  f, err := os.OpenFile("testlogfile", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
-  if err != nil {
-      log.Fatalf("error opening file: %v", err)
-  }
-  // defer f.Close()
-
-  log.SetOutput(f)
-
-  go GoogleIt()
-  for {
-    f, err := os.OpenFile("test.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-    if err != nil {
-        log.Fatal(err)
-    }
-    if _, err := f.Write([]byte("appended some data\n")); err != nil {
-        log.Fatal(err)
-    }
-    if err := f.Close(); err != nil {
-        log.Fatal(err)
-    }
-    time.Sleep(time.Second)
-  }
-
-  // never happen, but need to complete code
-  return usage, nil
-}
-
 func init() {
     f, err := os.OpenFile("text.log",
       os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
@@ -216,28 +145,28 @@ func init() {
 }
 
 func main() {
-  // flag.Usage = Usage
-  // address := flag.String("address", "127.0.0.1:9980", "Sia's API address")
-  // flag.StringVar(&password, "password", "", "Sia's API password")
-  // agent := flag.String("agent", "Sia-Agent", "Sia agent")
+  flag.Usage = Usage
+  address := flag.String("address", "127.0.0.1:9980", "Sia's API address")
+  flag.StringVar(&password, "password", "", "Sia's API password")
+  agent := flag.String("agent", "Sia-Agent", "Sia agent")
   // flag.BoolVar(&archive, "archive", false, "Files will not be removed from Sia, even if they are deleted locally")
-  // flag.BoolVar(&debug, "debug", false, "Enable debug mode. Warning: generates a lot of output.")
+  flag.BoolVar(&debug, "debug", false, "Enable debug mode. Warning: generates a lot of output.")
   // flag.StringVar(&prefix, "subfolder", "siasync", "Folder on Sia to sync files too")
   // flag.StringVar(&include, "include", "", "Comma separated list of file extensions to copy, all other files will be ignored.")
-  flag.StringVar(&daemon_cmd, "daemon", "none", "use one of the daemon commands: install | remove | start | stop | status")
+  // flag.StringVar(&daemon_cmd, "daemon", "none", "use one of the daemon commands: install | remove | start | stop | status")
   // flag.StringVar(&exclude, "exclude", "", "Comma separated list of file extensions to skip, all other files will be copied.")
   // flag.Uint64Var(&dataPieces, "data-pieces", 10, "Number of data pieces in erasure code")
   // flag.Uint64Var(&parityPieces, "parity-pieces", 30, "Number of parity pieces in erasure code")
   //
-  // flag.Parse()
+  flag.Parse()
   //
-  // sc := sia.New(*address)
-  // sc.Password = findApiPassword()
-  // sc.UserAgent = *agent
+  sc := sia.New(*address)
+  sc.Password = findApiPassword()
+  sc.UserAgent = *agent
   // directory := os.Args[len(os.Args)-1]
   //
   // // Verify that we can talk to Sia and have valid contracts.
-  // testConnection(sc)
+  testConnection(sc)
   //
   // includeExtensions = strings.Split(include, ",")
   // excludeExtensions = strings.Split(exclude, ",")
@@ -254,28 +183,14 @@ func main() {
   // signal.Notify(done, os.Interrupt)
   // <-done
   // fmt.Println("caught quit signal, exiting...")
-  f, err := os.OpenFile("testlogfile", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
-  if err != nil {
-      log.Fatalf("error opening file: %v", err)
-  }
-  defer f.Close()
 
-  log.SetOutput(f)
   y := YamlSync{}
   yamlFile, err := ioutil.ReadFile("test.yml")
+  if err != nil {
+    log.Fatalf("error: %v", err)
+  }
   yaml.Unmarshal(yamlFile, &y)
   fmt.Printf("%+v\n", y.Sync)
 
-  srv, err := daemon.New(name, description, dependencies...)
-  if err != nil {
-      errlog.Println("Error: ", err)
-      os.Exit(1)
-  }
-  service := &Service{srv}
-  status, err := service.Manage()
-  if err != nil {
-      errlog.Println(status, "\nError: ", err)
-      os.Exit(1)
-  }
-  fmt.Println(status)
+
 }
